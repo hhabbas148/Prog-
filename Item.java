f5c8462c38fa89b9399223fd9ceea2efaf62caaa
Hayder <119615667+hhabbas148@users.noreply.github.com>
2025-04-06 14:13:50 +0200

    item

--- Item.java
+++ Item.java
@@ -1,6 +1,13 @@
 public class Item {
+
+    private final String name;
+
+    public Item(String name) {
+        this.name = name;
+    }
+
     public String getName()
     {
-        return "hello";
+        return  name;
     }
 }
